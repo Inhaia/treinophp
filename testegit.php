1fb43650b29b28a3e6939b2b3b558efb5152eacf
unknown <cinthia.sinhaia@CAS411453W1029.senacsp.edu.br>
2016-03-16 20:48:11 -0300

    teste php

---
+++ testegit.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Coding Standards";
+
+$nome = 'cinthia';
+
+$nota = 2 + 6;
+
+$nome_completo = 'Cinthia da Silva' . 'Inhaia';
+
+$nome_completo .= 'Ritzel';
+
+echo'<br><br> Nome:'.$nome_completo;
+
+?>
